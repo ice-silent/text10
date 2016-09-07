914eecab4a65010f1e7eec2374d75bffd01c74e3
ice-silent <fumingzhen8@163.com>
2016-09-07 10:38:05 +0800

    掌握在两个Activity之间如何传递数据，其中数据有String、int等多种格式。

---
+++ app/src/main/java/com/example/fumingzhen/text10/AnotherActivity.java
@@ -0,0 +1,30 @@
+package com.example.fumingzhen.text10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age",20);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+        Button button=(Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AnotherActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
